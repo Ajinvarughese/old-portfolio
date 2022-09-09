b4dd1ca0d9bd28ee3d71c06df79d5b1a3d1933bb
Ajin Varughese <92434245+Ajinvarughese@users.noreply.github.com>
2022-09-09 21:08:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+    SMOOTH SCROLL FUNCTION
+*/
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+let clicked = 0
+function close_menu(){
+    clicked = 1
+    document.getElementById("menu-bar").style.left = "110%";   
+}
+
+function open_menu() {
+
+    if (clicked === 1) {
+
+        document.getElementById("menu-bar").style.left = "0%";      
+    }
+}
+
+
